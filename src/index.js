4ccea94ec4006323da0da3afa3568ff609d26835
mtsantiago1230 <105756992+mtsantiago1230@users.noreply.github.com>
2023-01-10 09:29:41 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App/index.js';
+import './index.css';
+
+
+const root = createRoot(document.getElementById('root'));
+root.render( <App />);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
